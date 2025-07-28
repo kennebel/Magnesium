42d4c13754a34be0d6ee5cffd11b4773153ec01c
Jeremy A. Cunningham <jac@kennebel.com>
2025-07-28 16:31:12 -0600

    take out debugging statement

--- scenes/World/WorldControl.cs
+++ scenes/World/WorldControl.cs
@@ -3,40 +3,39 @@ using System;
 
 public partial class WorldControl : Node
 {
-    [Export]
-    public PackedScene WireframeCube { get; set; }
-    [Export]
-    public Node3D UIElementsHolder { get; set; }
+	[Export]
+	public PackedScene WireframeCube { get; set; }
+	[Export]
+	public Node3D UIElementsHolder { get; set; }
 
-    public Node3D ClickedOn{ get; set; }
-    public CubeWireframeControl SelectionIndicator { get; set; }
+	public Node3D ClickedOn{ get; set; }
+	public CubeWireframeControl SelectionIndicator { get; set; }
 
-    public override void _Ready()
-    {
-        SelectionIndicator = WireframeCube.Instantiate<CubeWireframeControl>();
-        UIElementsHolder.AddChild(SelectionIndicator);
-        SelectionIndicator.Visible = false;
-    }
+	public override void _Ready()
+	{
+		SelectionIndicator = WireframeCube.Instantiate<CubeWireframeControl>();
+		UIElementsHolder.AddChild(SelectionIndicator);
+		SelectionIndicator.Visible = false;
+	}
 
-    public override void _Process(double delta)
-    {
-    }
+	public override void _Process(double delta)
+	{
+	}
 
-    public void TileClicked(Node3D clicked)
-    {
-        GD.Print("Clicked nameD: ", clicked.Name);
-        if (clicked == ClickedOn)
-        {
-            ClickedOn = null;
-            SelectionIndicator.Visible = false;
-        }
-        else
-        {
-            ClickedOn = clicked;
-            SelectionIndicator.GlobalPosition = ClickedOn.GlobalPosition;
-            SelectionIndicator.Visible = true;
+	public void TileClicked(Node3D clicked)
+	{
+		if (clicked == ClickedOn)
+		{
+			ClickedOn = null;
+			SelectionIndicator.Visible = false;
+		}
+		else
+		{
+			ClickedOn = clicked;
+			SelectionIndicator.GlobalPosition = ClickedOn.GlobalPosition;
+			SelectionIndicator.Visible = true;
 
-            SelectionIndicator.Offset = ClickedOn is GroundTileControl;
-        }
-    }
+			SelectionIndicator.Offset = ClickedOn is GroundTileControl;
+		}
+	}
 }
